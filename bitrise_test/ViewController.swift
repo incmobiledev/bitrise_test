96471d58c5259360de2116a1b6f9006e80614cc0
incmobiledev <mobiledevelopment@incomm.com>
2017-05-19 09:28:22 -0400

    init commit

---
+++ bitrise_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bitrise_test
+//
+//  Created by Joshua Gresham on 5/19/17.
+//  Copyright © 2017 Incomm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
