0ca800a5d98e03c70e6796e40647513d8d24af5e
incmobiledev <mobiledevelopment@incomm.com>
2017-05-22 07:58:20 -0400

    custom build script testing

---
+++ scripts/build.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import sys, json, urllib, os
+build_dir = './build'
+archive_path = build_dir + '/Archive'
+archive_file = archive_path + '.xcarchive'
+export_plist = './configs/Ad-Hoc.plist'
+
+args = ''
+args += '-project bitrise_test.xcodeproj '
+args += '-scheme Release '
+args += '-configuration release '
+args += '-archivePath ./build/Archive '
+args += 'archive '
+os.system('xcodebuild ' + args)
+
+args = ''
+args += '-verbose '
+args += '-exportArchive '
+args += '-archivePath ' + archive_file + ' '
+args += '-exportPath ' + build_dir + ' '
+args += '-exportOptionsPlist ' + export_plist + ' '
+os.system('xcodebuild ' + args)
