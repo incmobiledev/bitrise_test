06da1fa0518a6c6521bf5f76f4652392183ebb11
incmobiledev <mobiledevelopment@incomm.com>
2017-05-26 10:43:44 -0400

    master

--- bitrise_test/ViewController.swift
+++ bitrise_test/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let nextView = UIViewController();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = .green;
+        nextView.view.backgroundColor = .red;
+        
+        let button = UIButton();
+        button.addTarget(self, action: #selector(buttonTouch(sender:)), for: .touchUpInside);
+        let width:CGFloat = 80.0;
+        let height:CGFloat = 30.0;
+        let x = self.view.frame.width/2 - width/2;
+        let y = self.view.frame.height/2 - height/2;
+        button.setTitle("button", for: .normal);
+        button.frame = CGRect(x: x, y: y, width: width, height: height)
+        self.view.addSubview(button);
     }
-
+    
+    func buttonTouch(sender:UIButton) {
+        self.present(nextView, animated: true, completion: nil);
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
